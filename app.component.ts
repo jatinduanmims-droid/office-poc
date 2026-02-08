d71e9c452b53ca8a6e970abca7279a434ec875c0
Jatin <jatindua.nmims@gmail.com>
2026-02-08 20:50:07 +0530

    Update app.component.ts

--- app.component.ts
+++ app.component.ts
@@ -1,21 +1,17 @@
-import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { MenuItem } from 'primeng/api';
 
-import { EmailService } from './services/email.service';
-
-// Angular Material
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { MatDialog } from '@angular/material/dialog';
 import { MatSidenavModule, MatSidenav } from '@angular/material/sidenav';
+import { MatDialog } from '@angular/material/dialog';
 
-// Chatbot
 import { ChatbotDialogComponent } from './components/chatbot-dialog/chatbot-dialog.component';
+import { EmailService } from './services/email.service';
 
 @Component({
   selector: 'app-root',
@@ -23,13 +19,13 @@ import { ChatbotDialogComponent } from './components/chatbot-dialog/chatbot-dial
   imports: [
     CommonModule,
     RouterModule,
-    ChatbotDialogComponent,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
     MatMenuModule,
     MatExpansionModule,
-    MatSidenavModule
+    MatSidenavModule,
+    ChatbotDialogComponent
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
@@ -37,11 +33,10 @@ import { ChatbotDialogComponent } from './components/chatbot-dialog/chatbot-dial
 })
 export class AppComponent implements OnInit {
 
-  @ViewChild('chatSidenav') chatSidenav!: MatSidenav;
+  @ViewChild('chatSidenav') private chatSidenav!: MatSidenav;
 
-  menuItems: MenuItem[] = [];
-  tradeMenuOpen = false;
   leftPanelOpen = true;
+  tradeMenuOpen = false;
   chatOpen = false;
 
   constructor(
@@ -49,18 +44,7 @@ export class AppComponent implements OnInit {
     private dialog: MatDialog
   ) {}
 
-  ngOnInit(): void {
-    this.menuItems = [
-      { label: '', icon: 'pi pi-home', routerLink: ['/emails'] },
-      {
-        label: 'Trade Controls',
-        items: [
-          { label: 'Incoming Requests Management Control', routerLink: ['/dashboard'] },
-          { label: 'L/C Maturity follow up', routerLink: ['/lc-maturity'] }
-        ]
-      }
-    ];
-  }
+  ngOnInit(): void {}
 
   toggleLeftPanel(): void {
     this.leftPanelOpen = !this.leftPanelOpen;
@@ -72,11 +56,11 @@ export class AppComponent implements OnInit {
 
   openChatbot(): void {
     this.chatOpen = true;
-    this.chatSidenav.open();
+    this.chatSidenav?.open();
   }
 
   closeChatBot(): void {
     this.chatOpen = false;
-    this.chatSidenav.close();
+    this.chatSidenav?.close();
   }
 }
